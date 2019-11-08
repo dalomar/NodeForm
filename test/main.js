269dd71722614e31f4ac0ec6f6e5e28ae0b953d4
user.name like : dalomar <user.email like : benoumar41@hotmail.fr>
2019-11-08 14:07:48 +0100

    Added a first running test for sleepsort

---
+++ test/main.js
@@ -0,0 +1,12 @@
+// test/main.js
+var should = require('should');
+var sleepsort = require('../lib/main');
+
+describe('sleepsort', function() {
+    describe('with no arguments', function() {
+        it('returns an empty array', function() {
+            var result = sleepsort();
+            result.should.eql([]);
+        });
+    });
+});
